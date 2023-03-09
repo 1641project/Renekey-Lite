8dc6d35c364b5565c7e10b499d73765f21878350
taiyme <53635909+taiyme@users.noreply.github.com>
2023-03-09 21:33:57 +0900

    [refs #89] fix types: config.ts

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,5 +1,5 @@
 const address = new URL(location.href);
-const siteName = (document.querySelector('meta[property="og:site_name"]') as HTMLMetaElement)?.content;
+const siteName = document.querySelector<HTMLMetaElement>('meta[property="og:site_name"]')?.content;
 
 export const host = address.host;
 export const hostname = address.hostname;
@@ -8,12 +8,16 @@ export const apiUrl = url + '/api';
 export const wsUrl = url.replace('http://', 'ws://').replace('https://', 'wss://') + '/streaming';
 export const lang = localStorage.getItem('lang');
 export const langs = _LANGS_;
-export let locale = JSON.parse(localStorage.getItem('locale'));
+// TODO: any潰す
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export let locale = JSON.parse(localStorage.getItem('locale') || '{}') as any;
 export const version = _VERSION_;
 export const instanceName = siteName === 'Misskey' ? host : siteName;
 export const ui = localStorage.getItem('ui');
 export const debug = localStorage.getItem('debug') === 'true';
 
-export function updateLocale(newLocale) {
+// TODO: any潰す
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function updateLocale(newLocale: any): void {
 	locale = newLocale;
 }
