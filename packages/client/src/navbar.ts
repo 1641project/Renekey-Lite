1c865db1c9c21af65c910be5ab1832bdc27e17a1
taiyme <53635909+taiyme@users.noreply.github.com>
2023-07-20 14:24:23 +0900

    enhance navbar.ts

--- packages/client/src/navbar.ts
+++ packages/client/src/navbar.ts
@@ -1,5 +1,5 @@
 import { computed, reactive } from 'vue';
-import { $i } from './account';
+import { $i } from '@/account';
 import * as os from '@/os';
 import { i18n } from '@/i18n';
 import { ui } from '@/config';
@@ -7,95 +7,102 @@ import { unisonReload } from '@/scripts/unison-reload';
 
 export const navbarItemDef = reactive({
 	notifications: {
-		title: 'notifications',
+		title: i18n.ts.notifications,
 		icon: 'ti ti-bell',
 		show: computed(() => $i != null),
 		indicated: computed(() => $i != null && $i.hasUnreadNotification),
 		to: '/my/notifications',
 	},
 	messaging: {
-		title: 'messaging',
+		title: i18n.ts.messaging,
 		icon: 'ti ti-messages',
 		show: computed(() => $i != null),
 		indicated: computed(() => $i != null && $i.hasUnreadMessagingMessage),
 		to: '/my/messaging',
 	},
 	drive: {
-		title: 'drive',
+		title: i18n.ts.drive,
 		icon: 'ti ti-cloud',
 		show: computed(() => $i != null),
 		to: '/my/drive',
 	},
 	followRequests: {
-		title: 'followRequests',
+		title: i18n.ts.followRequests,
 		icon: 'ti ti-user-plus',
 		show: computed(() => $i != null && $i.isLocked),
 		indicated: computed(() => $i != null && $i.hasPendingReceivedFollowRequest),
 		to: '/my/follow-requests',
 	},
 	explore: {
-		title: 'explore',
+		title: i18n.ts.explore,
 		icon: 'ti ti-hash',
 		to: '/explore',
 	},
 	announcements: {
-		title: 'announcements',
+		title: i18n.ts.announcements,
 		icon: 'ti ti-speakerphone',
 		indicated: computed(() => $i != null && $i.hasUnreadAnnouncement),
 		to: '/announcements',
 	},
 	search: {
-		title: 'search',
+		title: i18n.ts.search,
 		icon: 'ti ti-search',
 		to: '/search',
 	},
+	// lookup: {
+	// 	title: i18n.ts.lookup,
+	// 	icon: 'ti ti-world-search',
+	// 	action: (ev) => {
+	// 		lookup();
+	// 	},
+	// },
 	lists: {
-		title: 'lists',
+		title: i18n.ts.lists,
 		icon: 'ti ti-list',
 		show: computed(() => $i != null),
 		to: '/my/lists',
 	},
 	antennas: {
-		title: 'antennas',
+		title: i18n.ts.antennas,
 		icon: 'ti ti-antenna',
 		show: computed(() => $i != null),
 		to: '/my/antennas',
 	},
 	favorites: {
-		title: 'favorites',
+		title: i18n.ts.favorites,
 		icon: 'ti ti-star',
 		show: computed(() => $i != null),
 		to: '/my/favorites',
 	},
 	pages: {
-		title: 'pages',
+		title: i18n.ts.pages,
 		icon: 'ti ti-news',
 		to: '/pages',
 	},
 	gallery: {
-		title: 'gallery',
+		title: i18n.ts.gallery,
 		icon: 'ti ti-icons',
 		to: '/gallery',
 	},
 	groups: {
-		title: 'groups',
+		title: i18n.ts.groups,
 		icon: 'ti ti-users',
 		show: computed(() => $i != null),
 		to: '/my/groups',
 	},
 	clips: {
-		title: 'clip',
+		title: i18n.ts.clip,
 		icon: 'ti ti-paperclip',
 		show: computed(() => $i != null),
 		to: '/my/clips',
 	},
 	channels: {
-		title: 'channel',
+		title: i18n.ts.channel,
 		icon: 'ti ti-device-tv',
 		to: '/channels',
 	},
 	ui: {
-		title: 'switchUi',
+		title: i18n.ts.switchUi,
 		icon: 'ti ti-devices',
 		action: (ev: MouseEvent) => {
 			const el = ev.currentTarget ?? ev.target;
@@ -124,10 +131,16 @@ export const navbarItemDef = reactive({
 		},
 	},
 	reload: {
-		title: 'reload',
+		title: i18n.ts.reload,
 		icon: 'ti ti-refresh',
 		action: () => {
 			location.reload();
 		},
+	},
+	profile: {
+		title: i18n.ts.profile,
+		icon: 'ti ti-user',
+		show: computed(() => $i != null),
+		to: `/@${$i?.username}`,
 	},
 });
