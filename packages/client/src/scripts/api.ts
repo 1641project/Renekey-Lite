9bd19ab2891ce1182351a2dc33108958e1f1c302
taiyme <53635909+taiyme@users.noreply.github.com>
2023-08-13 20:47:16 +0900

    fix types api/apiGet

--- packages/client/src/scripts/api.ts
+++ packages/client/src/scripts/api.ts
@@ -3,23 +3,63 @@ import { ref } from 'vue';
 import { apiUrl } from '@/config';
 import { $i } from '@/account';
 
+type IsNeverType<T> = [T] extends [never] ? true : false;
+type StrictExtract<Union, Cond> = Cond extends Union ? Union : never;
+type IsCaseMatched<
+	E extends keyof Endpoints,
+	P extends Endpoints[E]['req'],
+	C extends number,
+> = IsNeverType<StrictExtract<Endpoints[E]['res']['$switch']['$cases'][C], [P, any]>> extends false ? true : false;
+type GetCaseResult<
+	E extends keyof Endpoints,
+	P extends Endpoints[E]['req'],
+	C extends number,
+> = StrictExtract<Endpoints[E]['res']['$switch']['$cases'][C], [P, any]>[1];
+
+type APIResultType<
+	E extends keyof Endpoints,
+	P extends Endpoints[E]['req'],
+> = (
+	Endpoints[E]['res'] extends {
+		$switch: {
+			$cases: [any, any][];
+			$default: any;
+		};
+	} ? (
+			IsCaseMatched<E, P, 0> extends true ? GetCaseResult<E, P, 0>
+		: IsCaseMatched<E, P, 1> extends true ? GetCaseResult<E, P, 1>
+		: IsCaseMatched<E, P, 2> extends true ? GetCaseResult<E, P, 2>
+		: IsCaseMatched<E, P, 3> extends true ? GetCaseResult<E, P, 3>
+		: IsCaseMatched<E, P, 4> extends true ? GetCaseResult<E, P, 4>
+		: IsCaseMatched<E, P, 5> extends true ? GetCaseResult<E, P, 5>
+		: IsCaseMatched<E, P, 6> extends true ? GetCaseResult<E, P, 6>
+		: IsCaseMatched<E, P, 7> extends true ? GetCaseResult<E, P, 7>
+		: IsCaseMatched<E, P, 8> extends true ? GetCaseResult<E, P, 8>
+		: IsCaseMatched<E, P, 9> extends true ? GetCaseResult<E, P, 9>
+		: Endpoints[E]['res']['$switch']['$default']
+	) : Endpoints[E]['res']
+);
+
 export const pendingApiRequestsCount = ref(0);
 
 // Implements Misskey.api.ApiClient.request
-export const api = <E extends keyof Endpoints, P extends Endpoints[E]['req']>(
+export const api = <
+	E extends keyof Endpoints,
+	P extends Endpoints[E]['req'],
+>(
 	endpoint: E,
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	data: P = {} as any,
 	token?: string | null | undefined,
 	signal?: AbortSignal,
-): Promise<Endpoints[E]['res']> => {
+): Promise<APIResultType<E, P>> => {
 	pendingApiRequestsCount.value++;
 
 	const onFinally = (): void => {
 		pendingApiRequestsCount.value--;
 	};
 
-	const promise = new Promise<Endpoints[E]['res'] | void>((resolve, reject) => {
+	const promise = new Promise<APIResultType<E, P> | void>((resolve, reject) => {
 		// Append a credential
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		if ($i) (data as any).i = $i.token;
@@ -55,12 +95,14 @@ export const api = <E extends keyof Endpoints, P extends Endpoints[E]['req']>(
 };
 
 // Implements Misskey.api.ApiClient.request
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const apiGet = <E extends keyof Endpoints, P extends Endpoints[E]['req']>(
+export const apiGet = <
+	E extends keyof Endpoints,
+	P extends Endpoints[E]['req']
+>(
 	endpoint: E,
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	data: P = {} as any,
-): Promise<Endpoints[E]['res']> => {
+): Promise<APIResultType<E, P>> => {
 	pendingApiRequestsCount.value++;
 
 	const onFinally = (): void => {
@@ -70,7 +112,7 @@ export const apiGet = <E extends keyof Endpoints, P extends Endpoints[E]['req']>
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const query = new URLSearchParams(data as any);
 
-	const promise = new Promise<Endpoints[E]['res'] | void>((resolve, reject) => {
+	const promise = new Promise<APIResultType<E, P> | void>((resolve, reject) => {
 		// Send request
 		window.fetch(`${apiUrl}/${endpoint}?${query}`, {
 			method: 'GET',
